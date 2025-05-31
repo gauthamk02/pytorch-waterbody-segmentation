8c100dd3eebf194fa2a5da6e325d24d04a1c83c5
Gautham Krishnan <gauthamkrishnanpriya@gmail.com>
2025-05-31 13:49:56 +0530

    refactor: code and upgrade gradio

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import gradio as gr
-from functions import *
+import onnxruntime as rt
+from functions import run_inference
+
+model_path = 'weights/model.onnx'
+session = rt.InferenceSession(model_path)
+input_name = session.get_inputs()[0].name
+output_name = session.get_outputs()[0].name
 
 title = "Water Body Segmentation - Image Segmentation PyTorch"
 examples = ['examples/image1.png', 'examples/image2.png', 'examples/image3.png', 'examples/image4.png', 'examples/image5.png']
 
-interface = gr.Interface(fn=predict, inputs=gr.Image(type= 'numpy').style(height= 256),
-            outputs= gr.Image(type = "numpy").style(height= 256),
-            examples= examples, title= title, css= '.gr-box {background-color: rgb(230 230 230);}')
+def inference_wrapper(image):
+    return run_inference(image, session, input_name, output_name)
+
+
+interface = gr.Interface(fn=inference_wrapper, 
+            inputs=gr.Image(type='numpy', height=400, width=400),
+            outputs=gr.Image(type="numpy", height=400, width=400),
+            examples=examples, 
+            title=title)
 
 interface.launch()
--- functions.py
+++ functions.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import onnxruntime as rt
 
 def resize_preserve_aspect_ratio(image, size):
     h, w = image.shape[:2]
@@ -10,19 +9,13 @@ def resize_preserve_aspect_ratio(image, size):
         image = cv2.resize(image, (size, size * h // w))
     return image
 
-def predict(inp_image):
-
-    model_path = 'weights/model.onnx'
+def run_inference(inp_image, session, input_name, output_name):
     inp_dim = inp_image.shape[:2]
 
     image = cv2.resize(inp_image, (256, 256))
     image = np.array(image, dtype=np.float32) / 255.0
     image = np.transpose(image, (2, 0, 1))
     image = np.expand_dims(image, axis=0)
-
-    session = rt.InferenceSession(model_path)
-    input_name = session.get_inputs()[0].name
-    output_name = session.get_outputs()[0].name
 
     pred_onx = session.run([output_name], {input_name: image.astype(np.float32)})[0]
     pred_onx = pred_onx > 0.5
@@ -32,5 +25,5 @@ def predict(inp_image):
     pred_onx = np.expand_dims(pred_onx, axis=2)
     pred_onx = np.concatenate((pred_onx, pred_onx, pred_onx), axis=2)
 
-    output = resize_preserve_aspect_ratio(pred_onx, 256)
+    output = resize_preserve_aspect_ratio(pred_onx, 400)
     return output
